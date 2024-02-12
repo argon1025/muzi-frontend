3632d2ef9c3e604564992023424f7778c02c3ffa
argon1025 <argon1025@gmail.com>
2024-02-12 23:02:21 +0900

    feat: 네비바 컴포넌트 작성

--- src/components/main-navigation/main-navigation.tsx
+++ src/components/main-navigation/main-navigation.tsx
@@ -1,6 +1,17 @@
+import {ReactComponent as Logo} from '../../resources/logo.svg'
+
 const MainNavigation = ()=> {
     return (
-        <div>nav</div>
+        <div>
+        <div className="flex justify-center h-16 w-full px-2.5 py-5">
+            <div className='flex items-center w-full max-w-7xl'>
+            <Logo className='mr-2' /><span className="text-2xl text-sky-400 align-middle">체험단무지</span>
+            </div>
+        </div>
+        <div className='flex w-full h-9 bg-sky-100 justify-center items-center'>
+            <span className='text-sky-400 text-base'>블로그 체험단의 모든것을 모아보는 체험단무지</span>
+        </div>
+        </div>
     )
 }
 
