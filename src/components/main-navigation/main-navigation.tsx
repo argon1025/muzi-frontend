108dc73ee06083ac6e9de78e9fe40c5bf8325737
argon1025 <argon1025@gmail.com>
2024-02-13 14:03:56 +0900

    feat: 로고 링크 삽입

--- src/components/main-navigation/main-navigation.tsx
+++ src/components/main-navigation/main-navigation.tsx
@@ -4,8 +4,11 @@ const MainNavigation = ()=> {
     return (
         <div>
         <div className="flex justify-center h-16 w-full px-2.5 py-5">
-            <div className='flex items-center w-full max-w-7xl'>
-            <Logo className='mr-2' /><span className="text-2xl text-sky-400 align-middle">체험단무지</span>
+            <div className='w-full max-w-7xl'>
+                <div className='flex items-center w-full max-w-7xl cursor-pointer' onClick={()=> window.location.href = '/'}>
+                    <Logo className='mr-2' /><span className="text-2xl text-sky-400 align-middle">체험단무지</span>
+                </div>
+                <div className='ml-auto'></div>
             </div>
         </div>
         <div className='flex w-full h-9 bg-sky-100 justify-center items-center'>
