6e50afa8b615fa7697e9efef1b2857a4e08c5c2a
argon1025 <argon1025@gmail.com>
2024-02-18 15:51:34 +0900

    feat: 푸터 컴포넌트 추가

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="flex flex-col w-full h-[219px] bg-sky-400 mt-auto items-center">
+      <div className="flex flex-col w-full max-w-5xl mt-10 px-[10px]">
+        <div className="text-white text-2xl">체험단무지</div>
+        <div className="text-white text-sm mt-2">muzi@mu-zi.net</div>
+        <div className="text-white text-sm mt-10">©2024 MU-ZI, Inc. All rights reserved.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
